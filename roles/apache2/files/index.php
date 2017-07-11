29637637e9112c287c7071e6f8f2f575c13f9c76
Jasodeep Chatterjee <jasodeepchatterjee@gmail.com>
2017-07-11 22:47:32 +0530

    :wrench: apache2 installation

---
+++ roles/apache2/files/index.php
@@ -0,0 +1,4 @@
+<img src="https://s3.amazonaws.com/media-p.slid.es/uploads/77212/images/1762786/ansible_logo_black_square.png" style="height: 100px;width: 200px;margin: 0 auto;display: table;"></img>
+<?php
+	phpinfo();
+?>
